4dbe7eda317eb138474b66e0f3959eb958519e3f
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-07-18 19:37:29 +0100

    A dealt card is no longer in the pack

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,5 +1,5 @@
 class Deck
-  attr_reader :pack
+  attr_reader :pack, :deal_card, :remove_from_pack
   def initialize
   @pack = ['A♠', '2♠', '3♠',
     '4♠', '5♠', '6♠',
@@ -17,5 +17,11 @@ class Deck
     '3♦','4♦', '5♦', '6♦',
     '7♦', '8♦', '9♦',
     '10♦', 'J♦', 'Q♦', 'K♦']
+  end
+  def deal_card
+    random_card = @pack.sample(1)
+  end
+  def remove_from_pack(random_card)
+    @pack.delete(random_card)
   end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,7 +1,7 @@
 require 'deck'
 describe Deck do
+  let(:deck) { Deck.new }
   it 'has a pack of available cards' do
-    deck = Deck.new
     expect(deck.pack).to eq ['A♠', '2♠', '3♠',
     '4♠', '5♠', '6♠',
     '7♠', '8♠', '9♠',
@@ -18,5 +18,10 @@ describe Deck do
     '3♦','4♦', '5♦', '6♦',
     '7♦', '8♦', '9♦',
     '10♦', 'J♦', 'Q♦', 'K♦']
+  end
+  it "#remove_from_pack removes the dealt card from the pack" do
+    random_card = 'K♥'
+    deck.remove_from_pack(random_card)
+    expect(deck.pack.include?(random_card)).to eq false
   end
 end
